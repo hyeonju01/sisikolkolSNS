878a12f9f39179fbd0b632694ec0d6e84d9604d9
hyeonju01 <tangled6648@gmail.com>
2023-03-22 18:57:31 +0900

    Chores: @SpringBootTest 어노테이션 주석 처리

--- src/test/java/com/hyeon/snsproject/SisiKolkolApplicationTests.java
+++ src/test/java/com/hyeon/snsproject/SisiKolkolApplicationTests.java
@@ -3,7 +3,7 @@ package com.hyeon.snsproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SisiKolkolApplicationTests {
 
     @Test
